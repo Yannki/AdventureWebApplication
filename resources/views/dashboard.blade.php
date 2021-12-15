6b5c6aef41e4a873880d183c2efab6d9af77c12c
Yannki <Jankaji@inmail.sk>
2021-12-15 16:38:32 +0000

    dashboard - commissions display

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
         </div>
     </div>
 
-    <div class="py-12">
+    <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-green-200">
@@ -28,6 +28,28 @@
                     </ul>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <ul>
+                @foreach (Auth::user()->adventurer->commissions as $commission)
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-green-200 hover:bg-green-50">
+                        <li>
+                            <a href="/taverns/{{$commission->id}}"
+                            class="hover:text-indigo-800 
+                                   hover:text-underline text-right h-10 p-2 md:h-auto md:p-4">
+                            <br>{{$commission->name}} 
+                            <br>{{$commission->difficulty}} 
+                            <br>{{$commission->reward}}
+                            </a>
+                        </li>
+                    </div>
+                </div>
+                @endforeach
+            </ul> 
         </div>
     </div>
 </x-app-layout>
